53be1cdd58fbedcdba38323f030d1286435b8b2f
Samuel Tran <77118149+SamuelTRan-2712@users.noreply.github.com>
2022-11-01 18:04:34 -0400

    Create Process.java

---
+++ programming ass 2 my version/Process.java
@@ -0,0 +1,31 @@
+public class Process {
+	//for process ID
+	public int processID;
+	
+	//for burst time
+	public int burstTime;
+	
+	//for arrival time
+	public int arrivalTime;
+	
+	//to check if the process already started
+	public boolean ifProcessAlreadyStarted; 
+	
+	//for user name
+	public String userName;
+	
+	
+	/**
+	 * constructor
+	 * it needs to have such arguments so when we
+	 * create an instance process, we can pass its information (the arguments)
+	 * to the listOfAllProcesses
+	 */ 
+	public Process(int processID, int burstTime, int arrivalTime, String userName) {
+		this.processID = processID;
+		this.burstTime = burstTime;
+		this.arrivalTime = arrivalTime;
+		this.ifProcessAlreadyStarted = false;
+		this.userName = userName;
+	}
+}
