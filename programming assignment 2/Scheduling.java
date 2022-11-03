890dfa3ed4fe4fade4368b8908d10e8651594db9
Samuel Tran <77118149+SamuelTRan-2712@users.noreply.github.com>
2022-11-03 10:09:09 -0400

    Update Scheduling.java

--- programming assignment 2/Scheduling.java
+++ programming assignment 2/Scheduling.java
@@ -18,6 +18,10 @@ public class Scheduling {
 	//to keep track of CPU running time 
 	public int clock;
 	
+	public int numberOfActiveUsers;
+	
+	public ArrayList<String> activeUserNames;
+	
 	//public int currentRunningTime;
 	
 	//constructor
@@ -27,6 +31,8 @@ public class Scheduling {
 		this.readyQueue = new LinkedList<Process>();
 		this.listOfAllProcesses = new ArrayList<Process>();
 		this.clock = 1;
+		this.numberOfActiveUsers = 0;
+		this.activeUserNames = new ArrayList<String>();
 	}
 	
 	public void addProcess(int processID, String userName, String defaultProcess) {
@@ -52,13 +58,68 @@ public class Scheduling {
 		
 	}
 	
+	
 	public void addProcessesToReadyQueue(ArrayList<Process>listOfAllProcesses) {
+		int timePerUser = 0; 
+		ArrayList<Integer> timePerProcessPerUser = new ArrayList<Integer>();
 		for (Process process: listOfAllProcesses) {
 			if(process.arrivalTime <= clock) {
+				readyQueue.clear();
 				readyQueue.addLast(process);
+				
+				//this loop check the # of users that has processes that are ready to 
+				//calculate the time each user will receive per cycle
+				for(int i = 0; i < readyQueue.size(); i++ ) {
+					int counterSameUserInList = 0;
+					
+					//to add the first user in ready queue
+					if(activeUserNames.size() ==  0) {
+						activeUserNames.add(readyQueue.get(0).userName);
+					}
+					
+					//to check if the user is already in the active users list
+					// if yes, then dont add 
+					for(int j = 0; j < activeUserNames.size(); j++) {
+						if(activeUserNames.get(j) == readyQueue.get(i).userName) {
+							counterSameUserInList++; 	
+						}
+					}
+					
+					//if no, then add it 
+					if(counterSameUserInList == 0) {
+						activeUserNames.add(readyQueue.get(i).userName);
+					}
+				}
+				
+				timePerUser = cyclicQuantum/activeUserNames.size();
+				
+				//traverse through each user in the activeUserNames
+				for(int i = 0; i < activeUserNames.size(); i++) {
+					int countProcessesInUser = 0;
+					
+					//check how many processes there are in each active user  
+					for(int j = 0; j < readyQueue.size(); j++) {
+						if(activeUserNames.get(i) == readyQueue.get(j).userName) {
+							countProcessesInUser++;
+						}
+					}
+					
+					//adds the time each process will run for per user 
+					int timePerProcessForAUser = timePerUser/countProcessesInUser;
+					timePerProcessPerUser.add(timePerProcessForAUser);
+				}
+				
+			
+			
+				
+				
+				
 			}
 		}
 	}
 	
+	public void runProcess(LinkedList<Process>readyQueue) {
+		
+		
+	}
 	
-}
