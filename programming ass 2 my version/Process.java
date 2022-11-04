588b1bbd9cd1cd3456b8631ccf1db7f15084b558
Samuel Tran <77118149+SamuelTRan-2712@users.noreply.github.com>
2022-11-04 17:48:35 -0400

    Update Process.java

--- programming ass 2 my version/Process.java
+++ programming ass 2 my version/Process.java
@@ -1,34 +1,67 @@
-public class Process {
-	//for process ID
-	public int processID;
-	
-	//for burst time
-	public int burstTime;
-	
-	//for arrival time
-	public int arrivalTime;
-	
-	//to check if the process already started
-	public boolean ifProcessAlreadyStarted; 
-	
-	//for user name
-	public String userName;
-	
-	//Thread thread;
-	
-	
-	/**
-	 * constructor
-	 * it needs to have such arguments so when we
-	 * create an instance process, we can pass its information (the arguments)
-	 * to the listOfAllProcesses
-	 */ 
-	public Process(int processID, int burstTime, int arrivalTime, String userName) {
-		this.processID = processID;
-		this.burstTime = burstTime;
-		this.arrivalTime = arrivalTime;
-		this.ifProcessAlreadyStarted = false;
-		this.userName = userName;
-		//this.thread = new Thread();
-	}
+public class Process extends Thread {
+    /**
+     * Process ID
+     */
+    public int processId;
+
+    /**
+     * Process user
+     */
+    public String user;
+
+    /**
+     * Process arrival time
+     */
+    public int arrivalTime;
+
+    /**
+     * Process burst time
+     */
+    public int burstTime;
+
+    /**
+     * Says if the process has already been started
+     */
+    public boolean isStarted;
+    
+    Thread thread;
+
+    /**
+     * Process constructor
+     * 
+     * @param processId   Process ID
+     * @param user        Process user - From the input file
+     * @param arrivalTime Process arrival time - From the input file
+     * @param burstTime   Process burst time - From the input file
+     */
+    public Process(int processId, String user, int arrivalTime, int burstTime) {
+        this.processId = processId;
+        this.user = user;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.isStarted = false;
+        this.thread = new Thread();
+    }
+
+    /**
+     * Process toString() method for printing
+     */
+    public String toString() {
+        return this.user + this.processId + " " + this.arrivalTime + " " + this.burstTime;
+    }
+    
+    public void run() {
+    	System.out.println("Process " + this.user + this.processId +", started");
+    	while (this.burstTime > 0) {
+    		try {
+    			this.burstTime -- ;
+    			thread.sleep(1000);
+    		}
+    		catch(InterruptedException e) {
+    			System.out.println("Process " + this.user + this.processId +", paused");
+    		}
+    		
+    	}
+    	System.out.println("Process " + this.user + this.processId + ", resumed");
+    }
 }
